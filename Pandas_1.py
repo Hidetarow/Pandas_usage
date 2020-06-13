cbc1aadbe8ec64457281646735f1567a7fbc8b4a
hidetarow <54745725+Hidetarow@users.noreply.github.com>
2020-06-13 17:11:54 +0900

    test 1

---
+++ Pandas_1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+d0 = ['a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'c']
+d1 = np.random.rand(9)
+d_t = np.array([d0, d1]).T
+df = pd.DataFrame(d_t, columns=['lot', 'col_1'])
+df.set_index('lot', inplace=True)
+print(df)
+#iiiprint(df)print(df)print(df)print(df)print(df)
